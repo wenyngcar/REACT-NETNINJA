d4f760b19f0001f96217aa54790f94dd8473350d
Owen Cariño <owenyoungcarino@gmail.com>
2024-06-11 16:51:57 +0800

    style: replace box-shadow on blog container instead of border.

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -9,7 +9,10 @@ const BlogList = ({ blogList }) => {
     <div className="blog-list space-y-5">
       {/* Single Blog Container */}
       {blogList.map((blog) => (
-        <div key={blog.id} className="p-3 hover:border-b-2 ">
+        <div
+          key={blog.id}
+          className="p-3 hover:shadow-lg transition duration-200"
+        >
           <p className="text-xl font-bold text-purple-500">{blog.title}</p>
           <p>{blog.body}</p>
         </div>
