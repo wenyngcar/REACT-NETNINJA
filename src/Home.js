e16cdde4ae47a4975c974dacea602c30e939b085
Owen Cariño <owenyoungcarino@gmail.com>
2024-06-11 19:15:46 +0800

    style: animate-spin on loading

--- src/Home.js
+++ src/Home.js
@@ -11,8 +11,19 @@ const Home = () => {
   return (
     <div className="home">
       <div className=" text-2xl font-semibold py-6">Home Page</div>
-      {error && <div>{error}</div>}
-      {isPending && <div>Loading...</div>}
+      {error && <div className="text-5xl">{error}</div>}
+      {isPending && (
+        <div className="flex items-center text-5xl">
+          <svg class="animate-spin h-10 w-10 mr-5 ..." viewBox="0 0 24 24">
+            <path
+              class="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+          Loading...
+        </div>
+      )}
       {/* Blogs Container */}
       {blogs && <BlogList blogList={blogs} />} {/* If there is blogs */}
     </div>
