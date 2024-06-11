31a820fb40ece9d2bf74f392428d4f2d70e8e96c
Owen Cariño <owenyoungcarino@gmail.com>
2024-06-11 17:03:18 +0800

    feat: set Router on App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import Home from "./components/home";
+import Home from "./components/Home";
 import Navbar from "./components/navbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="container mx-auto px-20 ">
-      <Navbar />
-      <Home />
-    </div>
+    <Router>
+      <div className="container mx-auto px-20 ">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
