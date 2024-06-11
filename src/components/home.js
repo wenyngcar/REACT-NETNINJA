aef1e286188cb52ef10f7bc632322de43a8c16be
Owen Cariño <owenyoungcarino@gmail.com>
2024-06-11 16:01:02 +0800

    feat: conditional loading messages

--- src/components/home.js
+++ src/components/home.js
@@ -3,6 +3,7 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
   const deleteBlog = (id) => {
     /* Returns filtered array and does not affect original array */
@@ -20,12 +21,15 @@ const Home = () => {
       .then((data) => {
         /* Actual data after parsing the response object. */
         setBlogs(data);
+        setIsPending(false);
       });
   }, []);
+  /* Fire only once on render if dependencies is empty. */
 
   return (
     <div className="home">
       <div className=" text-2xl font-semibold py-6">Home Page</div>
+      {isPending && <div>Loading...</div>}
       {/* Blogs Container */}
       {blogs && <BlogList blogList={blogs} deleteBlog={deleteBlog} />}{" "}
       {/* If there is blogs */}
