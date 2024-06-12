8f114a2241141adfb3df4a0946a938595d8bae07
Owen Cariño <owenyoungcarino@gmail.com>
2024-06-12 12:12:37 +0800

    feat: handleSubmit

--- src/Create.js
+++ src/Create.js
@@ -4,12 +4,20 @@ const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body };
+    console.log(blog);
+  };
+
   return (
-    <div className="mx-auto text-center py-3 w-96">
-      <div className="p-5 text-xl font-semibold text-red-500">Add a blog</div>
+    <div className="mx-auto py-3 w-96">
+      <div className="p-5 text-xl font-semibold text-red-500 text-center">
+        Add a blog
+      </div>
       <div>
-        <form className="space-y-4">
-          <div className="flex gap-x-3 items-center">
+        <form className="space-y-4" onSubmit={handleSubmit}>
+          <div className="">
             <label className="text-lg">Title</label>
             <input
               className="outline-none border-2 w-full rounded-md px-2 py-1"
@@ -19,8 +27,8 @@ const Create = () => {
               onChange={(e) => setTitle(e.target.value)}
             ></input>
           </div>
-          <div className="flex gap-x-3">
-            <label className="text-lg">Body</label>
+          <div className="">
+            <label className="text-lg ">Body</label>
             <textarea
               placeholder="Enter blog body..."
               className="outline-none border-2 w-full rounded-md px-2 py-1 min-h-52 h-52"
@@ -28,6 +36,9 @@ const Create = () => {
               onChange={(e) => setBody(e.target.value)}
             ></textarea>
           </div>
+          <button className="border border-black w-full px-2 py-1 rounded-md hover:text-white hover:bg-red-500 hover:border-red-500 transition duration-150">
+            Add Blog
+          </button>
         </form>
       </div>
     </div>
