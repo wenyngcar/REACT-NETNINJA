fd96994917d0bcf50470d3748844b84f87524f79
Owen Cariño <owenyoungcarino@gmail.com>
2024-06-12 11:36:32 +0800

    feat: form template for creating blog

--- src/Create.js
+++ src/Create.js
@@ -1,5 +1,29 @@
+import { useState } from "react";
+
 const Create = () => {
-  return <div>Create a new Blog</div>;
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  return (
+    <div className="mx-auto text-center py-3 w-96">
+      <div className="p-5 text-xl font-semibold text-red-500">Add a blog</div>
+      <div>
+        <form className="space-y-4">
+          <div className="flex gap-x-3 items-center">
+            <label className="text-lg">Title</label>
+            <input
+              className="outline-none border-2 w-full rounded-md px-2 py-1"
+              type="text"
+            ></input>
+          </div>
+          <div className="flex gap-x-3">
+            <label className="text-lg">Body</label>
+            <textarea className="outline-none border-2 w-full rounded-md px-2 py-1 min-h-52 h-52"></textarea>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Create;
